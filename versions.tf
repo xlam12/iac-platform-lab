7f51a3d5fb371e48d21492028fae55841291b5f7
Xuan Lam <14982045+xlam12@users.noreply.github.com>
2025-11-18 14:13:36 -0800

    Complete a real Terraform project skeleton

---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_version = ">= 1.9.0"
+    
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "~> 5.50"                     # current stable major version
+
+        }
+    }
+}
